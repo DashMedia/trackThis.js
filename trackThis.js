0e565518e16481c7a7250cd7d5d1c855eb67103c
Joanthan Haslett <jon@haslett.id.au>
2013-01-15 09:07:19 +0930

    Commented out form submit code. Appears to have a higher accuracy rate now.

--- trackThis.js
+++ trackThis.js
@@ -5,10 +5,10 @@ function trackThis(){
 	$('a:not([href^="tel:"]).trackThis').each(function(){
 		$(this).click(function(){
 			var arguments = $(this).attr('data-trackThis').split(',');
-			_gaq.push(['_trackEvent', arguments[0], arguments[1], arguments[2], parseInt(arguments[3])]);		
-			_gaq.push(function(){
-			    return true;
-			});
+			_gaq.push(
+				['_trackEvent', arguments[0], arguments[1], arguments[2], parseInt(arguments[3])]
+			);
+			return true;
 		});
 	}); 
 	// END <a>" selector.
@@ -20,37 +20,41 @@ function trackThis(){
 			$(this).click(function(){
 				var arguments = $(this).attr('data-trackThis').split(',');
 				if($(this).hasClass('clicked')){
-					_gaq.push(['_trackEvent', arguments[0], 'Clicked', arguments[2], 0]);		
-					_gaq.push(function(){
-						return true;
-					});
+					_gaq.push(
+						['_trackEvent', arguments[0], 'Clicked', arguments[2], 0]
+					);	
+					return true;
 				}else{
 					$(this).addClass('clicked');
 					$(this).html(phoneNumber);
-					_gaq.push(['_trackEvent', arguments[0], 'Revealed', arguments[2], parseInt(arguments[3])]);		
-					return false;
+					_gaq.push(
+						['_trackEvent', arguments[0], 'Revealed', arguments[2], parseInt(arguments[3])]
+					);
+					return false;		
 				}
 			});
 		}else{
 			$(this).click(function(){
 				var arguments = $(this).attr('data-trackThis').split(',');
-				_gaq.push(['_trackEvent', arguments[0], arguments[1], arguments[2], parseInt(arguments[3])]);		
-				_gaq.push(function(){
-			    return true;
-			  });
+				_gaq.push(
+					['_trackEvent', arguments[0], arguments[1], arguments[2], parseInt(arguments[3])]
+				);		
+				return true;
 			});//end function
 		}//end else
 	}); 
 	// END <a href="tel:"> selector.
 	//START input[type="submit"]
 	$('input[type="submit"].trackThis').each(function(){
-		$(this).submit(function(){
+		/*
+$(this).submit(function(){
 			var arguments = $(this).attr('data-trackThis').split(',');
-			_gaq.push(['_trackEvent', arguments[0], arguments[1], arguments[2], parseInt(arguments[3])]);		
-			_gaq.push(function(){
-		    return true;
-		  });
+			_gaq.push(
+				['_trackEvent', arguments[0], arguments[1], arguments[2], parseInt(arguments[3])]
+			);
+			return true;
 		});
+*/
 	}); 
 	// END input[type="submit"] selector.
 }//END initTrackThis();
